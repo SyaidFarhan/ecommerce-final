2062958a62c7b6a73170e2a960eefd36e2da989e
syaidt <syaidtengku@gmail.com>
2026-01-10 10:46:50 +0700

    Add force-dynamic to product listing pages and update API services

--- src/app/product/listing/Sports/page.js
+++ src/app/product/listing/Sports/page.js
@@ -1,6 +1,8 @@
 import CommonListing from "@/components/CommonListing";
 import { productByCategory } from "@/services/product";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Sports () {
   const getAllProducts = await productByCategory("sportsEquipment");
 
